f50bdc5bf56111a9a8b73c6f4deae67267593ba1
Ryan Phung <ryan.phung@gmail.com>
2023-12-13 17:18:10 +0800

    Vocabs and tokens up to chap 16

--- src/data/tokens/index.js
+++ src/data/tokens/index.js
@@ -1,18 +1,38 @@
 import tokens0 from './tokens-0.json'
 import tokens1 from './tokens-1.json'
 import tokens2 from './tokens-2.json'
+import tokens3 from "./tokens-3.json"
+import tokens4 from "./tokens-4.json"
+import tokens5 from "./tokens-5.json"
 import tokens6 from './tokens-6.json'
 import tokens7 from './tokens-7.json'
+import tokens8 from "./tokens-8.json";
+import tokens9 from "./tokens-9.json";
+import tokens10 from "./tokens-10.json";
+import tokens11 from "./tokens-11.json";
+import tokens12 from "./tokens-12.json";
+// import tokens13 from "./tokens-13.json";
+import tokens14 from "./tokens-14.json";
+import tokens15 from "./tokens-15.json";
 
 const data = [
   tokens0,
   tokens1,
   tokens2,
-  undefined,
-  undefined,
-  undefined,
+  tokens3,
+  tokens4,
+  tokens5,
   tokens6,
-  tokens7
-]
+  tokens7,
+  tokens8,
+  tokens9,
+  tokens10,
+  tokens11,
+  tokens12,
+  // tokens13,
+  undefined,
+  tokens14,
+  tokens15,
+];
 
 export default data
